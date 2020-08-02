a23b6fca24b3e0fa0cb82f04986ae577000862e2
KhushalAbrol <khushalabrol14@gmail.com>
2020-08-02 11:04:19 +0530

    Added Database using Mongoose

---
+++ NodeJS/node-mongoose/index.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+
+    console.log('Connected correctly to server');
+
+    var newDish = Dishes({
+        name: 'Uthappizza',
+        description: 'test'
+    });
+
+    newDish.save()
+        .then((dish) => {
+            console.log(dish);
+
+            return Dishes.find({});
+        })
+        .then((dishes) => {
+            console.log(dishes);
+
+            return Dishes.remove({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+});
+/*      To start MongoDB :- */
+/*      mongod --dbpath=data --bind_ip 127.0.0.1    */
